4630ce6a8c47aa8cc1b16ff94a28f9b224bf9e2a
Thomas Achleitner <2110405+DLoT@users.noreply.github.com>
2026-03-23 12:45:55 +0100

    use target musl

--- flake.nix
+++ flake.nix
@@ -18,7 +18,13 @@
       overlays = [ (import rust-overlay) ];
       pkgs = import nixpkgs { inherit system overlays; };
 
+      aarch64-musl-pkgs = pkgs.pkgsCross.aarch64-multiplatform-musl;
+
       rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        targets = [
+          "x86_64-unknown-linux-musl"
+          "aarch64-unknown-linux-musl"
+        ];
         extensions = [
           "rust-src"
           "rust-analyzer"
@@ -28,12 +34,13 @@
     {
       devShells.${system}.default = pkgs.mkShell {
         RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+        CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "aarch64-unknown-linux-musl-gcc";
 
         buildInputs = with pkgs; [
           rustToolchain
+          aarch64-musl-pkgs.buildPackages.gcc
           pkg-config
           yarn
-          napi-rs-cli
         ];
 
         shellHook = ''
